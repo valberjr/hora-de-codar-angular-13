fba995ef79c8f7dfa6889c77383fd9636adf2180
v4lb3r <3cifhzht@duck.com>
2022-04-21 16:11:06 -0300

    compartilhamento de dados - @Input()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-13-curso';
+
+  userName = 'Viviane';
+  userData = {
+    email: 'viviane@email.com',
+    role: 'admin',
+  };
 }
