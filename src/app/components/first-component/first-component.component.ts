5a3a9293b716422017f53a48cbe07344ab91842c
v4lb3r <3cifhzht@duck.com>
2022-04-21 15:47:32 -0300

    dados no template - interpolação

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.scss']
+  styleUrls: ['./first-component.component.scss'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Jéssica';
+  age: number = 30;
+  job: string = 'Programador';
+  hobbies = ['Correr', 'Jogar', 'Estudar'];
+  car = { name: 'argo', year: 2022 };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
