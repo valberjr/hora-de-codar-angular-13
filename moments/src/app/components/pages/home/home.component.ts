f77148926455ad6892d6eb41701e2461ec263291
valberjr <3cifhzht@duck.com>
2022-05-25 15:51:05 -0300

    busca de dados no angular

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -15,7 +15,8 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
 
-  // todo search
+  faSearch = faSearch;
+  searchTerm = '';
 
   constructor(private momentService: MomentService) {}
 
@@ -30,5 +31,14 @@ export class HomeComponent implements OnInit {
       this.allMoments = data;
       this.moments = data;
     });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value)
+    );
   }
 }
